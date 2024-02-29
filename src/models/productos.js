ebe585e7700d4fbf5faa3eb56ff47a119097d713
feliciano389 <felixx-21@hotmail.com>
2024-02-29 01:50:32 -0500

    quien sabe

--- src/models/productos.js
+++ src/models/productos.js
@@ -1,50 +1,39 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../database/config');
 
-
 const Productos = sequelize.define('productos', {
   id_producto: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
-    primaryKey: true
+    primaryKey: true,
   },
   nombre: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true,
   },
-
   descripcion: {
     type: DataTypes.STRING,
-    allowNull: false, 
+    allowNull: false,
   },
-
   precioCosto: {
-    type: DataTypes.DECIMAL(10, 2),
+    type: DataTypes.DOUBLE,
     allowNull: true,
-    defaultValue: 0, 
+    defaultValue: 0,
   },
-  
-
-
   precioVenta: {
     type: DataTypes.DOUBLE,
     allowNull: true,
-    defaultValue: 0, 
+    defaultValue: 0,
   },
-  
   stock: {
     type: DataTypes.INTEGER,
     allowNull: true,
     defaultValue: 0,
   },
-
   estado: {
     type: DataTypes.ENUM('Activo', 'Inactivo'),
     defaultValue: 'Activo',
   },
 });
-
-
 
 module.exports = Productos;
