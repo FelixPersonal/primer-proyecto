f9a4969b0a42568497189c105747f58a79e54537
valecarsua <valeriazaarez.12@gmail.com>
2024-03-05 21:17:10 -0500

    ventas

--- src/controllers/ventas.js
+++ src/controllers/ventas.js
@@ -23,7 +23,7 @@ const getVentas = async (req, res = response) => {
 
 const getVenta = async (req, res = response) => {
 
-  const id_ventas = req.params;
+  const id_ventas = req.params.id;
 
   try {
 
