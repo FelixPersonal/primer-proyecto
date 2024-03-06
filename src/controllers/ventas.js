5f0a76a21aaac71a223c7ff3582badd9eb500c48
valecarsua <valeriazaarez.12@gmail.com>
2024-03-05 19:27:07 -0500

    ventas

--- src/controllers/ventas.js
+++ src/controllers/ventas.js
@@ -22,7 +22,7 @@ const getVentas = async (req, res = response) => {
 
 
 const getVenta = async (req, res = response) => {
-  const id_ventas = req.params.id_ventas;
+  const id_ventas = req.params;
   try {
     const venta = await Venta.findByPk(id_ventas);
 
