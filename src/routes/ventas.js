827452d41a0e48401c4481231a84cd962b9612b9
valecarsua <valeriazaarez.12@gmail.com>
2024-03-05 21:26:30 -0500

    ventas

--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -6,7 +6,7 @@ const verificarToken = require('../middlewares/verificarToken');
 const { postVentas, getVentas, cancelarVenta, cambiarEstado, getVenta } = require('../controllers/ventas');
 
 route.get('/venta', verificarToken, getVentas);
-route.get('/venta/:id_ventas', verificarToken, getVenta);
+route.get('/venta/:id', verificarToken, getVenta);
 route.post('/venta', verificarToken, postVentas);
 route.put('/venta/estadoventa/:id_ventas', verificarToken, cambiarEstado);
 route.put('/venta/cancelar/:id_ventas', verificarToken, cancelarVenta);
