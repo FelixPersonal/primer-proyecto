8c7540eed6529d199c02e2c6f0a0c96e64a5dc26
valecarsua <valeriazaarez.12@gmail.com>
2024-03-05 20:59:10 -0500

    ventas

--- src/controllers/ventas.js
+++ src/controllers/ventas.js
@@ -23,7 +23,7 @@ const getVentas = async (req, res = response) => {
 
 const getVenta = async (req, res = response) => {
 
-  const id_ventas = req.params.id;
+  const { id_ventas } = req.params.id;
 
   try {
 
@@ -32,7 +32,7 @@ const getVenta = async (req, res = response) => {
     if (ventas) {
       res.json(ventas);
     } else {
-      res.status(404).json({ error: `No se encontró ninguna venta con el documento: ${documento}` });
+      res.status(404).json({ error: `No se encontró ninguna venta con el documento: ${id_ventas}` });
     }
   } catch (error) {
     console.error(error); // Imprimir el error para obtener más detalles
