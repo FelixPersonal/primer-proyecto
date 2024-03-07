dae57bd478da587273c81e2aa99b677bbd05500b
valecarsua <valeriazaarez.12@gmail.com>
2024-03-07 08:53:04 -0500

    lista de abonos

--- src/controllers/abonos.js
+++ src/controllers/abonos.js
@@ -1,4 +1,16 @@
 const Abonos = require ('../models/abonos');
 const { response } = require('express');
 
-module.exports = {};
+const getAbonos = async (req, res = response) => {
+    try {
+        const abonos = await Abonos.findAll();
+        res.json ({ abonos });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al ontener los abonos.' });
+    }
+};
+
+module.exports = {
+    getAbonos
+};
--- src/routes/abonos.js
+++ src/routes/abonos.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const route = Router();
 const verificarToken = require('../middlewares/verificarToken');
 
-const {  } = require('../controllers/abonos');
+const { getAbonos } = require('../controllers/abonos');
+
+route.get('/abonos', verificarToken, getAbonos);
 
 module.exports = route;
