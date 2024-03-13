e51deaa45f50a3c78e57d14c50f001936d12195b
Puzzless <simon01ruiz@gmail.com>
2024-03-12 22:15:33 -0500

    Cambio en ruta citas

--- src/routes/citas.js
+++ src/routes/citas.js
@@ -7,7 +7,7 @@ const verificarToken = require('../middlewares/verificarToken');
 const { getCita, getCitas, getCitasServcios, getCitasHoy, postCita, putCitaEstado, putCita, deleteCita } = require('../controllers/citas');
 
 route.get('/citas',verificarToken, getCitas);
-route.get('/citas/servicios',verificarToken, getCitasServcios);
+route.get('/citas/servicios/:id',verificarToken, getCitasServcios);
 route.get('/citas/:id',verificarToken, getCita);
 route.post('/citashoy',verificarToken, getCitasHoy)
 route.post('/citas',verificarToken, postCita);
