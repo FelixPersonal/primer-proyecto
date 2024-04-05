9c80074c00cdab6c5a393faf8ca4a300d3591217
Puzzless <simon01ruiz@gmail.com>
2024-04-04 23:54:26 -0500

    Se agrega nueva funcion citas_servicios_indo

--- src/controllers/citas_servicios.js
+++ src/controllers/citas_servicios.js
@@ -11,6 +11,44 @@ const getCitasServicios = async (req, res = response) => {
   }
 }
 
+const getCitaServicioInfo = async () => {
+  try {
+    // Obtener todas las relaciones entre citas y servicios
+    const listCitasServicios = await CitasServicios.findAll();
+
+    // Crear un array para almacenar la información completa de cada cita y servicio
+    const citasServiciosInfo = [];
+
+    // Recorrer cada relación entre cita y servicio
+    for (const citaServicio of listCitasServicios) {
+      // Obtener el ID de la cita y el ID del servicio
+      const { id_cita, id_servicio } = citaServicio;
+
+      // Buscar la información completa de la cita utilizando su ID
+      const cita = await Citas.findByPk(id_cita);
+
+      // Buscar la información completa del servicio utilizando su ID
+      const servicio = await Servicios.findByPk(id_servicio);
+
+      // Si tanto la cita como el servicio existen, agregar la información al array de resultados
+      if (cita && servicio) {
+        citasServiciosInfo.push({
+          cita: cita,
+          servicio: servicio
+        });
+      }
+    }
+
+    // Devolver el array con la información completa de todas las citas y servicios relacionados
+    return citasServiciosInfo;
+    
+  } catch (error) {
+    console.error(error);
+    return { error: 'Error al obtener la lista de citas de servicios' };
+  }
+}
+
+
 const getCitaServicio = async (req, res = response) => {
   const { id } = req.params;
   try {
@@ -80,6 +118,7 @@ const deleteCitaServicio = async (req, res = response) => {
 module.exports = {
   getCitaServicio,
   getCitasServicios,
+  getCitaServicioInfo,
   postCitaServicio,
   putCitaServicio,
   deleteCitaServicio
