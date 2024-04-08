e268761f329f799b24a93ede59ae0c3e31c2ca98
Puzzless <simon01ruiz@gmail.com>
2024-04-08 10:18:09 -0500

    Se agregar permiso a cors

--- src/models/server.js
+++ src/models/server.js
@@ -40,7 +40,7 @@ class Server {
 
   middlewares() {
     const corsOptions = {
-      origin: 'http://localhost:3001',
+      origin: ['http://localhost:3001', 'https://proyectobac-de612.web.app'],
       credentials: true,
     };
 
